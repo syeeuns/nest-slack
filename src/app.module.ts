c3295101e8194410a0ed25928b9a27e1c01bf830
yeny <yeny@yenys-MacBook-Pro.local>
2022-11-01 23:28:35 +0900

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*');
+  }
+}
